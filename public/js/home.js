a5686fe121f40ad4c17eb1faa7a35ccbbd5ef03a
xocignacio <xose_ignacio@hotmail.com>
2022-10-13 12:57:47 -0300

    register_login

--- public/js/home.js
+++ public/js/home.js
@@ -3,3 +3,5 @@ const btn = document.getElementById('logoutbtn')
 btn.addEventListener('click',evt=>{
     fetch('/api/sessions/logout').then(result=>result.json()).then(json=>console.log(json));
 })
+
+
