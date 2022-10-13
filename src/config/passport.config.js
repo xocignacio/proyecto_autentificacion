b3408f3dba4a2efd6368205eac2e81b760fc9a37
xocignacio <xose_ignacio@hotmail.com>
2022-10-13 10:44:42 -0300

    arreglando_ando

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import local from 'passport-local';
 import userService from '../../models/Users.js'
+import { createHash, isValidPassword } from "../../utils.js";
 
 const localStrategy = local.Strategy; /// en local utilizo username + password
 
