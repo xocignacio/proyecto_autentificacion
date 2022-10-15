01fdda249eba4a67bc008e0201ffae343014f344
xocignacio <xose_ignacio@hotmail.com>
2022-10-15 17:57:07 -0300

    register_login

--- public/js/home.js
+++ public/js/home.js
@@ -4,4 +4,5 @@ btn.addEventListener('click',evt=>{
     fetch('/api/sessions/logout').then(result=>result.json()).then(json=>console.log(json));
 })
 
-
+alert(' ✨ Logeado con exito, Bienvenido 🎉')
+console.log ('Logeado con exito')
